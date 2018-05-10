f5912683d84ac13c2adf07d76aaf4e46d8ecbf0d
chinazhangyang <tonyfirst1@163.com>
2018-05-10 21:52:16 +0800

    fix

--- dataproc/get_weather.py
+++ dataproc/get_weather.py
@@ -1,13 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_city_weather(city, date):
-    
-    url = 'http://tianqi.2345.com/t/wea_history/js/201804/%s_%s.js'%(city, date)
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
-    return soup.string
 
+class weather_parser():
+    
+    def __init__(self, city, year_month):
+        self.__city = city
+        self.__year_month = year_month
+        
+    def value(self):
+        
+        url = 'http://tianqi.2345.com/t/wea_history/js/201804/%s_%s.js'%(self.__city, self.__year_month)
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        return soup.string
+
+
+class city_paser():
+    
+    def __init__(self, city):
+        self.__city = city
+    
+    def value(self):
+            return '56294'
+        
 if __name__ == '__main__':
     
-    print get_city_weather('56294', '201804')
+    print weather_parser(city_paser('').value(), '201804').value()
